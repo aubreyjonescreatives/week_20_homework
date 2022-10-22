55276d3b404a06284f9018aee1529bf652a366b9
Aubrey Jones <aubrey_jones07@hotmail.com>
2022-10-22 14:00:21 -0600

    init commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# This is my heading 
+
+
+***This is bolded text**
+
+
+`<h1>
+
+This is another header
+
+</h1>`
+
+
+!
---
+++ javascript/index.js
@@ -0,0 +1,37 @@
+var myFirstVariable = 'variableValue';
+
+console.log(myFirstVariable);
+
+
+function testNum(a) {
+    var result; 
+    if (a > 0) {
+    result = 'positive';
+} else {
+    result = 'Not positive';
+}
+return result; 
+
+}
+console.log(testNum(15))
+
+
+function whatIsMyFavoriteFood() {
+    var a = "cereal";
+    var response;
+    if (a = "eggs") {
+      response = "That's my favorite food!";
+    } else {
+      response = "No sir";
+    }
+  console.log(WhatIsMyFavoriteFood(response));
+  return response;
+  }
+  
+
+
+
+
+
+
+
